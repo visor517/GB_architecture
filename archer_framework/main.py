cd46d061382054809153e4afbe88e60e96c4b094
visor517 <visor517@yandex.ru>
2022-01-06 21:25:14 +0300

    added get post

--- archer_framework/main.py
+++ archer_framework/main.py
@@ -1,35 +1,59 @@
+from quopri import decodestring
+
+from .requests import PostRequests, GetRequests
+
+
 class PageNotFound404:
     def __call__(self, request):
-        return '404 WHAT', '404 PAGE Not Found'
+        return "404 WHAT", "404 PAGE Not Found"
 
 
 class Framework:
-
     def __init__(self, routes_obj, fronts_obj):
         self.routes_lst = routes_obj
         self.fronts_lst = fronts_obj
 
     def __call__(self, environ, start_response):
-        # получаем адрес, по которому выполнен переход
-        path = environ['PATH_INFO']
+        # получаем параметры запроса
+        path = environ["PATH_INFO"]
+        method = environ["REQUEST_METHOD"]
 
         # добавление закрывающего слеша
-        if not path.endswith('/'):
-            path = f'{path}/'
+        if not path.endswith("/"):
+            path = f"{path}/"
+
+        request = {"method": method}
+
+        if method == "GET":
+            data = GetRequests().get_request_params(environ)
+            request["params"] = data
+            if len(data) > 0:
+                print(f"Пришёл get-запрос с параметрами: {data}")
+        if method == "POST":
+            data = PostRequests().get_request_params(environ)
+            request["params"] = Framework.decode_value(data)
+            print(f"Пришёл post-запрос с параметрами: {Framework.decode_value(data)}")
 
         # находим нужный контроллер
-        # отработка паттерна page controller
         if path in self.routes_lst:
             view = self.routes_lst[path]
         else:
             view = PageNotFound404()
-        request = {}
-        # наполняем словарь request элементами
+
         # этот словарь получат все контроллеры
-        # отработка паттерна front controller
         for front in self.fronts_lst:
             front(request)
+
         # запуск контроллера с передачей объекта request
         code, body = view(request)
-        start_response(code, [('Content-Type', 'text/html')])
-        return [body.encode('utf-8')]
+        start_response(code, [("Content-Type", "text/html")])
+        return [body.encode("utf-8")]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace("%", "=").replace("+", " "), "UTF-8")
+            val_decode_str = decodestring(val).decode("UTF-8")
+            new_data[k] = val_decode_str
+        return new_data
---
+++ archer_framework/requests.py
@@ -0,0 +1,52 @@
+class GetRequests:
+
+    @staticmethod
+    def parse_input_data(data: str):
+        result = {}
+        if data:
+            params = data.split('&')
+            for item in params:
+                k, v = item.split('=')
+                result[k] = v
+        return result
+
+    def get_request_params(self, environ):
+        query_string = environ['QUERY_STRING']
+        data = self.parse_input_data(
+            query_string) if len(query_string) > 0 else {}
+        return data
+
+
+class PostRequests:
+
+    @staticmethod
+    def parse_input_data(data: str):
+        result = {}
+        if data:
+            params = data.split('&')
+            for item in params:
+                k, v = item.split('=')
+                result[k] = v
+        return result
+
+    @staticmethod
+    def get_wsgi_input_data(env) -> bytes:
+        content_length_data = env.get('CONTENT_LENGTH')
+        content_length = int(content_length_data) if content_length_data else 0
+        data = env['wsgi.input'].read(content_length) \
+            if content_length > 0 else b''
+        return data
+
+    def parse_wsgi_input_data(self, data: bytes) -> dict:
+        result = {}
+        if data:
+            data_str = data.decode(encoding='utf-8')
+            result = self.parse_input_data(data_str)
+        return result
+
+    def get_request_params(self, environ):
+        # получаем данные
+        data = self.get_wsgi_input_data(environ)
+        # собираем словарь
+        data = self.parse_wsgi_input_data(data)
+        return data
